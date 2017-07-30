771fde8587fd8d9a105d1d566272470a6d1c21e5
jalvar53 <joseluh_1@hotmail.com>
2017-07-30 10:48:30 -0500

    IP Address for new database at DCA

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cors = require('cors');
 app.use(cors());
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/webDB',
+mongoose.connect('mongodb://10.131.137.157/webDB',
     {useMongoClient: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
